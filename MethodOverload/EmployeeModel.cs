c89209611baf88630916afeba2ea167ca1b89c22
Pierre Plourde <pierre@spartancsharp.net>
2024-02-09 10:02:56 -0500

    Create Code
    Create an EmployeeModel class. Create three different constructors that take in different amounts of data.

---
+++ MethodOverload/EmployeeModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+	public class EmployeeModel
+	{
+		public string FirstName
+		{
+			get; set;
+		}
+
+		public string LastName
+		{
+			get; set;
+		}
+
+		public string Email
+		{
+			get; set;
+		}
+
+		public decimal Salary
+		{
+			get; set;
+		}
+
+		public string FullName
+		{
+			get
+			{
+				return $"{FirstName} {LastName}";
+			}
+		}
+
+		public EmployeeModel()
+		{
+		}
+
+		public EmployeeModel(string firstName, string lastName)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+		}
+
+		public EmployeeModel(string firstName, string lastName, string email)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Email = email;
+		}
+
+		public EmployeeModel(string firstName, string lastName, decimal salary)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Salary = salary;
+		}
+	}
+}
--- MethodOverload/Program.cs
+++ MethodOverload/Program.cs
@@ -10,6 +10,17 @@ namespace MethodOverload
 	{
 		static void Main(string[] args)
 		{
+			EmployeeModel employee1 = new EmployeeModel();
+			EmployeeModel employee2 = new EmployeeModel("Tim", "Corey");
+			EmployeeModel employee3 = new EmployeeModel("Tim", "Corey", "tim.corey@iamtimcorey.com");
+			EmployeeModel employee4 = new EmployeeModel("Tim", "Corey", 60000.00M);
+
+			Console.WriteLine($"{employee1.FullName}\t{employee1.Email}\t{employee1.Salary}");
+			Console.WriteLine($"{employee2.FullName}\t{employee2.Email}\t{employee2.Salary}");
+			Console.WriteLine($"{employee3.FullName}\t{employee3.Email}\t{employee3.Salary}");
+			Console.WriteLine($"{employee4.FullName}\t{employee4.Email}\t{employee4.Salary}");
+
+			_ = Console.ReadLine();
 		}
 	}
 }
